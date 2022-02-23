3f891488093fd63604de8dd0f6d94e89f0fcf383
shopify <mail@muizzahmed.com>
2022-02-23 10:09:35 +0000

    first-commit

---
+++ node_modules/avatax/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _AvaTaxClient = require('./lib/AvaTaxClient');
+
+var _AvaTaxClient2 = _interopRequireDefault(_AvaTaxClient);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _AvaTaxClient2.default; /**
+                                          * Avalara © 2017
+                                          * file: index.js
+                                          */
